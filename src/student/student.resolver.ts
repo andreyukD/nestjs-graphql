432e74f9c70da43cc2a942f1b1bd23251ef7e0ed
Dima <wushu_dima@mail.ru>
2021-01-27 22:23:21 +0100

    assigned students to lesson

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -13,36 +13,13 @@ export class StudentResolver {
     return this.studentService.createStudent(createStudentInput);
   }
 
-  // mutation {
-  //   createStudent(createStudentInput: {
-  //     firstName: "Dima",
-  //     lastName: "Andr"
-  //   }) {
-  //     id
-  //     firstName
-  //     lastName
-  //   }
-  // }
-
   @Query((returns) => [StudentType])
   async students() {
     return this.studentService.getStudents();
   }
 
-  // query {
-  //   students {
-  //     firstName
-  //   }
-  // }
-
   @Query((returns) => StudentType)
   student(@Args('id') id: string) {
     return this.studentService.getStudent(id);
   }
-
-  // query {
-  //   student(id: "639b6524-5164-43ed-ad5f-3ebe7d557560") {
-  //     firstName
-  //   }
-  // }
 }
